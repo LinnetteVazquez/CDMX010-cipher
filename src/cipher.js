e54d636e0324a69447c0550600d808719707d6b6
Linnette Vázquez Velazco <lin23newcvaz@gmail.com>
2020-12-02 22:29:25 -0600

    realice CSS y solo una funcion de js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,16 @@
 const cipher = {
-  // ... 
-};
+ 
+}
+  
 
+/* Formula del cifrado cesar
+  (x+n)%26
+  codigo ASCII 
+  (x[numero de la letra en la posicion del codigo ASCII]-65+n)%26+65
+*/
 export default cipher;
+ 
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import cipher from './cipher.js';
+let msg1 = document.getElementById("msg1");
+let despazamiento = document.getElementById("desplazamiento");
+let codificar = document.getElementById ("codificar");
+let decodificar = document.getElementById ("decodificar");
+let hoja1 = document.getElementById("nav");
+let resultado= document.getElementById ("resultado")
+
+
+codificar.addEventListener("click", function(){
+resultado.classList.remove("resultado")
+hoja1.style.display="none"
+}
+);
+
+
+decodificar.addEventListener("click", function(){
+resultado.classList.remove("resultado")
+hoja1.style.display="none"
+}
+);
+
 
 
 console.log(cipher);
