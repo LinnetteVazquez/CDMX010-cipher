679dea0c60a53e2a817a84c23844f3dd4598a4b0
LinnetteVazquez <lin23newcvaz@gmail.com>
2020-12-05 22:54:15 -0600

    Funciones para cifrar o descifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,56 @@
 const cipher = {
-codificar(msg1, desplazamiento) {
-  
-   
-}
- 
- 
+  //funcion que recibe argumentos
+  encode(msg1, offset) {
+    const textoMayusculas = msg1.toUpperCase();
+    // tipo de daro que va almacenar (string vacio)
+    let mensajeCifrado = "";
+    for (let i = 0; i < textoMayusculas.length; i++) {
+      //obteniendo c/letra
+      const letra = textoMayusculas[i];
+      //si letra es igual a un espacio
+      if (letra === " ") {
+        mensajeCifrado += " ";
+        //se salta todos los comandos sig. y empieza en la sig. letra.
+        continue;
+      }
+      //convirtiendo a num. ASCII
+      const asciiValor = letra.charCodeAt();
+      //hacer formula, cifra la letra
+      const letraCifrada = ((asciiValor - 65 + offset) % 26) + 65;
+      //prueba de consola
+      console.log({ letra, asciiValor, letraCifrada });
+      //cconvierte numero a letra
+      mensajeCifrado += String.fromCharCode(letraCifrada);
+    }
+    //retorno de la funcion
+    return mensajeCifrado;
+  },
 
-
+  decode(msg1, offset) {
+    const textoMayusculas = msg1.toUpperCase();
+    // tipo de daro que va almacenar (string vacio) 1°se define
+    let mensajeDescifrado = "";
+    for (let i = 0; i < textoMayusculas.length; i++) {
+      //obteniendo c/letra
+      const letra = textoMayusculas[i];
+      if (letra === " ") {
+        mensajeDescifrado += " ";
+        //se salta todos los comandos sig. y empieza en la sig. letra.
+        continue;
+      }
+      //convirtiendo a num. ASCII
+      const asciiValor = letra.charCodeAt();
+      //hacer formula, cifra la letra
+      const letraDescifrada = ((asciiValor + 65 - offset) % 26) + 65;
+      //prueba de consola
+      console.log({ letra, asciiValor, letraDescifrada });
+      //cconvierte numero a letra 2°se usa
+      mensajeDescifrado += String.fromCharCode(letraDescifrada);
+    }
+    //retorno de la funcion 3°se retorna
+    return mensajeDescifrado;
+  },
 };
-  
- 
-  
 
 /* Formula del cifrado cesar
   (x+n)%26
@@ -18,8 +58,3 @@ codificar(msg1, desplazamiento) {
   (x[numero de la letra en la posicion del codigo ASCII]-65+n)%26+65
 */
 export default cipher;
- 
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 let msg1 = document.getElementById("msg1").value;
-let desplazamiento = document.getElementById("desplazamiento").value;
-let codificar = document.getElementById ("codificar");
-let decodificar = document.getElementById ("decodificar");
+let offset = document.getElementById("offset").value;
+let codificar = document.getElementById("codificar");
+let decodificar = document.getElementById("decodificar");
 let hoja1 = document.getElementById("nav");
-let resultado= document.getElementById ("resultado")
+let resultado = document.getElementById("resultado");
 let msg2 = document.getElementById("msgCoD").value;
 let regresar = document.getElementById("regresar");
-    
 
-codificar.addEventListener("click", function(){
-resultado.classList.remove("resultado");
-hoja1.style.display="none";
-}
-);
+codificar.addEventListener("click", function () {
+  resultado.classList.remove("resultado");
+  hoja1.style.display = "none";
+});
 
+decodificar.addEventListener("click", function () {
+  resultado.classList.remove("resultado");
+  hoja1.style.display = "none";
+});
 
-decodificar.addEventListener("click", function(){
-resultado.classList.remove("resultado");
-hoja1.style.display="none";
-}
-);
+regresar.addEventListener("click", function () {
+  resultado.classList.add("resultado");
+  hoja1.style.display = "block";
+});
 
-regresar.addEventListener("click", function(){
-    resultado.classList.add("resultado");
-    hoja1.style.display="block";
-    }
-    );
-
-
-console.log(cipher);
+console.log(cipher.encode("hola mundo desciframe esta palabra", 13));
+console.log(cipher.decode("BYN ZHAQB QRFPVSENZR RFGN CNYNOEN", 13));
